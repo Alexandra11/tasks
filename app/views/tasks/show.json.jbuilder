993841d0a0fb360d5942a422c8f5195423d3c3c5
Alexandra Popovycova <olex.popovych@gmail.com>
2014-11-29 18:22:30 +0100

    Prvni verze  webu

---
+++ app/views/tasks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @task, :id, :name, :decription, :completed, :deadline, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_key_base = '334dabd6e6090ae27fcdf809b7c807358c4d6df1de1327212ace2a6145de2f2e0edde8b912748c42d6ee03cf5611d44603069298f318360cfc4aacf2866b3a28'
